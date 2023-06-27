095dbf532c240e9b42e12bec46b5d3c44dfcb44a
Janine Masunzu <jamasunzu99@gmail.com>
2023-06-27 14:08:48 -0500

    Exercise2 done

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
 
 namespace ArraysAndLists
 {
@@ -9,11 +11,15 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int Array and populate numbers 1-10
+            int[] myArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
             /* Create two Lists of type int.
              * Name one List "evens"
              * Name the other List "odds"
              */
+
+            var evens = new List<int>();
+            var odds = new List<int>();
 
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
@@ -21,12 +27,32 @@ namespace ArraysAndLists
              * Then add those numbers to either the evens List
              * or the odds List
              */
+           foreach (int item in myArray) 
+            {
+               if(item % 2  == 0)
+                { 
+                    evens.Add(item);
+                }
+                else 
+                {
+                    odds.Add(item);
+                }
+            }
 
             /* Now using foreach or for loops,
              * display each List of even and odd numbers
              *
              * Try to be creative in your display
              */
+            foreach (var evenNumbers in evens) 
+            { 
+                Console.WriteLine(evenNumbers);
+            }
+            foreach(var oddNumbers in odds) 
+            { 
+                Console.WriteLine(oddNumbers);
+            }
+                
         }
     }
 }
